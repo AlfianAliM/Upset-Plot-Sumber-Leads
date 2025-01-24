27c4c010933cc4cf4e5c147ce7ca27f551bdc037
Alfian Ali Murtadlo <alfian.ali@alumni.ui.ac.id>
2025-01-24 21:40:07 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from upsetplot import from_memberships, UpSet
+import matplotlib.pyplot as plt
+import streamlit as st
+
+st.title('Upset Plot Visualisasi Sumber Lead')
+
+def process_and_plot(df):
+    # ambil kolom kedua (kolom pertama diabaikan)
+    categories = df.columns[1:]
+    
+    # kombinasi dari kategori
+    df_combinations = pd.DataFrame({
+        "Combination": categories,
+        "Count": df.iloc[0, 1:].values  # Aambil nilai kecuali kolom 1
+    })
+    
+    # mengonversi data format memberships
+    upset_data = from_memberships(
+        memberships=[comb.split(", ") for comb in df_combinations["Combination"]],
+        data=df_combinations["Count"]
+    )
+    
+    #  upset plot
+    upset_plot = UpSet(upset_data, show_counts=True)
+    upset_plot.plot()
+    
+    # plot
+    st.pyplot(plt)
+
+# upload file CSV
+uploaded_file = st.file_uploader("Upload file CSV", type=["csv"])
+
+if uploaded_file is not None:
+    
+    df = pd.read_csv(uploaded_file)
+    
+    st.write("Data yang diupload:")
+    st.write(df)
+    
+    process_and_plot(df)
